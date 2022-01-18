049de31710a78168c66ae01059fa27f4656a9a51
Vitor Diniz <vitordiniz2010@hotmail.com>
2022-01-18 13:22:31 -0300

    add make_split

--- preprocessing/4mula_read.py
+++ preprocessing/4mula_read.py
@@ -24,13 +24,24 @@ class Processor:
     
     def make_split (self, tags):
         tags = tags.apply(lambda x: np.concatenate([i for i in x if i is not None]), axis=1)
-        tags = tags
-        top_50 = sorted(
-                    zip(*np.unique(np.concatenate(tags.values),return_counts=True)), 
-                    key=lambda x: x[1], reverse=True
-        )[:50]
-        top_50 = [i[0] for i in top_50]
-        
+        tags = tags.reset_index().drop_duplicates('index').reset_index(drop=True)
+        top_50 = np.load('../split/tags.npy')
+
+        print('getting ids on dataset...')
+        ids = [i.split('.')[0] for i in glob.glob(os.path.join(self.npy_path, '*.npy'))]
+        tags = tags.query('index in @ids')
+        print('making binary.npy file...')
+        binary = tags[0].map(lambda x: [int(i in x) for i in top_50])
+        binary = np.stack(binary).astype('float64')
+        print('splitting dataset...')
+        idx_shuff = tags.index.values.copy()
+        idx_shuff = np.random.shuffle(idx_shuff)
+        train,test,val = np.split(idx_shuff, [int(len(ids)*0.6), int(len(ids)*0.8)])
+        print('saving files...')
+        np.save('../split/binary.npy', binary)
+        np.save('../split/train.npy', tags.query('index in @train').apply(lambda x: f'{x.name}\t{x.values[0]}', axis=1).values.astype('U30'))
+        np.save('../split/test.npy', tags.query('index in @test').apply(lambda x: f'{x.name}\t{x.values[0]}', axis=1).values.astype('U30'))
+        np.save('../split/valid.npy', tags.query('index in @val').apply(lambda x: f'{x.name}\t{x.values[0]}', axis=1).values.astype('U30'))
 
     def iterate(self, data_path, output_path):
         self.get_paths(data_path, output_path)
@@ -40,4 +51,7 @@ class Processor:
             
 if __name__ == '__main__':
 	p = Processor()
-	fire.Fire({'run': p.iterate})
+	fire.Fire({
+        'run': p.iterate,
+        'split': p.make_split
+    })
