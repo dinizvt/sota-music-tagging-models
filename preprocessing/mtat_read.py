18c20a6bbdc1646f0a0a0ebc92cf1b920a42f0d1
Vitor Diniz <vitordiniz2010@hotmail.com>
2022-02-16 14:13:46 -0300

    add threading to preprocessing

--- preprocessing/mtat_read.py
+++ preprocessing/mtat_read.py
@@ -1,10 +1,11 @@
+import warnings
 import os
 import numpy as np
 import glob
 import librosa
 import fire
 import tqdm
-
+import threading
 
 class Processor:
 	def __init__(self):
@@ -20,19 +21,32 @@ class Processor:
 		x, sr = librosa.core.load(fn, sr=self.fs)
 		return x
 
+	def thread_routine (self, files, pbar):
+		with warnings.catch_warnings():
+			warnings.simplefilter('ignore')
+			for fn in files:
+				pbar.update()
+				npy_fn = os.path.join(self.npy_path, fn.split('/')[-1][:-3]+'npy')
+				if not os.path.exists(npy_fn):
+					try:
+						x = self.get_npy(fn)
+						np.save(open(npy_fn, 'wb'), x)
+					except RuntimeError:
+						# some audio files are broken
+						print(fn)
+						continue
+
 	def iterate(self, data_path):
 		self.get_paths(data_path)
-		for fn in tqdm.tqdm(self.files):
-			npy_fn = os.path.join(self.npy_path, fn.split('/')[-1][:-3]+'npy')
-			if not os.path.exists(npy_fn):
-				try:
-					x = self.get_npy(fn)
-					np.save(open(npy_fn, 'wb'), x)
-				except RuntimeError:
-					# some audio files are broken
-					print(fn)
-					continue
-
+		hw = len(self.files)//2
+		pbar = tqdm.tqdm(total=len(self.files))
+		left = threading.Thread(target=self.thread_routine, args = (self.files[hw:], pbar))
+		right = threading.Thread(target=self.thread_routine, args = (self.files[:hw], pbar))
+		left.start()
+		right.start()
+		left.join()
+		right.join()
+		
 if __name__ == '__main__':
 	p = Processor()
 	fire.Fire({'run': p.iterate})
